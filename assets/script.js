74c313df8998392e4ab254edb497b4886559ca4f
amanullayev2210 <karory577@gmail.com>
2025-01-22 21:06:02 +0500

    Add new functions

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+document.querySelectorAll('.hero_link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault(); 
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      // Custom scroll animatsiya
+      const startPosition = window.pageYOffset;
+      const targetPosition = targetSection.offsetTop;
+      const distance = targetPosition - startPosition;
+      const duration = 1350; 
+      let startTime = null;
+
+      function scrollAnimation(currentTime) {
+        if (!startTime) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+
+        if (timeElapsed < duration) {
+          requestAnimationFrame(scrollAnimation);
+        }
+      }
+
+      function ease(t, b, c, d) {
+        const easeInOutQuad = t => {
+          if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+          return (-c / 2) * (--t * (t - 2) - 1) + b;
+        };
+        return easeInOutQuad(t);
+      }
+
+      requestAnimationFrame(scrollAnimation);
+    });
+  });
+
+  document.querySelectorAll('.header_link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault(); 
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      // Custom scroll animatsiya
+      const startPosition = window.pageYOffset;
+      const targetPosition = targetSection.offsetTop;
+      const distance = targetPosition - startPosition;
+      const duration = 2650; 
+      let startTime = null;
+
+      function scrollAnimation(currentTime) {
+        if (!startTime) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+
+        if (timeElapsed < duration) {
+          requestAnimationFrame(scrollAnimation);
+        }
+      }
+
+      function ease(t, b, c, d) {
+        const easeInOutQuad = t => {
+          if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+          return (-c / 2) * (--t * (t - 2) - 1) + b;
+        };
+        return easeInOutQuad(t);
+      }
+
+      requestAnimationFrame(scrollAnimation);
+    });
+  });
